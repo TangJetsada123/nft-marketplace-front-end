56ab15551ede7edd3a8c7d6d419fba7ee55eccbb
ploy-thanaporn <tnpsp1501@gmail.com>
2022-11-10 16:13:51 +0700

    test

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,14 +1,16 @@
-import {Routes, Route, Navigate } from 'react-router-dom';
-import Home from '../page/home'
-import Market from '../page/market';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Create from "../page/Create";
+import Home from "../page/Home";
+import Market from "../page/Market";
 
-const  Routers = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Navigate to='/home'/>}/>
-            <Route path="/home" element={<Home />}/>
-            <Route path="/market" element={<Market />}/>
-        </Routes>
-    );
-}
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/market" element={<Market />} />
+      <Route path="/create" element={<Create />} />
+    </Routes>
+  );
+};
 export default Routers;
