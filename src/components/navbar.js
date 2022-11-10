528bc0dc9a0a3d907f14159a8c8efc78e807ef04
ploy-thanaporn <tnpsp1501@gmail.com>
2022-11-10 16:13:51 +0700

    test

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -11,9 +11,10 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import metamask from '../assets/images/metamask.png'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import WalletModal from "./ui/modal/wallet-modal";
 const Wallet = styled.button`
-    margin-left: 65%;
-`
+  margin-left: 65%;
+`;
 
 function Navbar() {
     const navigate = useNavigate();
@@ -205,9 +206,23 @@ function Navbar() {
                         {showLogin && <WalletModal setShowLogin={setShowLogin} setChoice={setChoice} />}
                     </div>
                 </div>
-            </nav>
+              </div>
+            </div>
+            <Wallet
+              className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+              onClick={clicked}
+            >
+              Connect Wallet
+            </Wallet>
+            {choice}
+            {showLogin && (
+              <WalletModal setShowLogin={setShowLogin} setChoice={setChoice} />
+            )}
+          </div>
         </div>
-    );
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
--- src/page/Create.js
+++ src/page/Create.js
@@ -1,12 +1,15 @@
-
-import React from 'react'
+import React from "react";
 
 const Create = () => {
-    return (
-        <div>
-          
-        </div>
-    );
-}
+  return (
+    <div className="my-24">
+      <h2 className="text-2xl font-bold">Create New Item</h2>
+      <div>
+        <p>Image, Video, Audio, or 3D Model</p>
+        <div className="w-[350px] h-[257px] bg-slate-50"></div>
+      </div>
+    </div>
+  );
+};
 
 export default Create;
