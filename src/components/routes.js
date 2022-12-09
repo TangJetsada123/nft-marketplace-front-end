ea0dac12dea3a92246ce4467208aaf1de30f8ec4
TangJetsada123 <jetsada_jinaka@hotmail.com>
2022-12-09 14:49:14 +0700

    feat: create asset

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,14 +1,16 @@
 import {Routes, Route, Navigate } from 'react-router-dom';
 import Home from '../page/home'
-import Market from '../page/market';
-import Account from '../page/account'
+import Explore from '../page/explore';
+import Account from '../page/account';
+import Create from '../page/create';
 const  Routers = () => {
     return (
         <Routes>
             <Route path="/" element={<Navigate to='/home'/>}/>
             <Route path="/home" element={<Home />}/>
-            <Route path="/market" element={<Market />}/>
+            <Route path="/market" element={<Explore />}/>
             <Route path="/account" element={<Account />}/>
+            <Route path="/create" element={<Create />}/>
         </Routes>
     );
 }
